37b7720d1ef5904841202c94ccdb589d57493437
mohammadObeidat <mohammadkhadeer1992@gmail.com>
2019-12-07 03:56:53 +0400

    create imagePath model

---
+++ app/src/main/java/ng/com/uae/kandora/model/ImagePath.java
@@ -0,0 +1,17 @@
+package ng.com.uae.kandora.model;
+
+public class ImagePath {
+    String imagePath;
+    public ImagePath(String imagePath)
+    {
+        this.imagePath = imagePath;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+}
