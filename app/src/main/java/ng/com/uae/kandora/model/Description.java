b5a31cf761da673769ae2dcbc85a79e3081d4ae4
mohammadObeidat <mohammadkhadeer1992@gmail.com>
2019-12-07 04:04:11 +0400

    craete commonComponents to use in other models

--- app/src/main/java/ng/com/uae/kandora/model/Description.java
+++ app/src/main/java/ng/com/uae/kandora/model/Description.java
@@ -1,26 +1,8 @@
 package ng.com.uae.kandora.model;
 
-public class Description {
-    String arabicDes,englishDes;
-    public Description (String arabicDes,String englishDes)
-    {
-        this.arabicDes = arabicDes;
-        this.englishDes = englishDes;
-    }
+public class Description extends CommonComponents{
 
-    public String getArabicDes() {
-        return arabicDes;
-    }
-
-    public void setArabicDes(String arabicDes) {
-        this.arabicDes = arabicDes;
-    }
-
-    public String getEnglishDes() {
-        return englishDes;
-    }
-
-    public void setEnglishDes(String englishDes) {
-        this.englishDes = englishDes;
+    public Description(String arabicDes, String englishDes) {
+        super(arabicDes, englishDes);
     }
 }
