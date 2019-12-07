6e39f2abcc16211c37d3f1988b748c034362e0d1
mohammadObeidat <mohammadkhadeer1992@gmail.com>
2019-12-08 00:43:12 +0400

    add kandora activity

---
+++ app/src/main/java/ng/com/uae/kandora/view/createKandora/CreateKandoraActivity.java
@@ -0,0 +1,18 @@
+package ng.com.uae.kandora.view.createKandora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ng.com.uae.kandora.R;
+
+public class CreateKandoraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_kandora);
+
+
+
+    }
+}
