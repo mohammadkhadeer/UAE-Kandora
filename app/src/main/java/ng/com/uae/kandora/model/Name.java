93a35efb56a9232dd9e3eb233ff020a436bcc1ad
mohammadObeidat <mohammadkhadeer1992@gmail.com>
2019-12-07 04:14:46 +0400

    craete itemName model

---
+++ app/src/main/java/ng/com/uae/kandora/model/Name.java
@@ -0,0 +1,7 @@
+package ng.com.uae.kandora.model;
+
+public class Name extends CommonComponents {
+    public Name(String arabicDes, String englishDes) {
+        super(arabicDes, englishDes);
+    }
+}
