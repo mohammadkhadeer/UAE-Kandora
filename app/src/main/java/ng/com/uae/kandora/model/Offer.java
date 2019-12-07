f2a2f1299fb26e394cd10e5017573abb0a9d6724
mohammadObeidat <mohammadkhadeer1992@gmail.com>
2019-12-07 04:21:07 +0400

    craete offer model

---
+++ app/src/main/java/ng/com/uae/kandora/model/Offer.java
@@ -0,0 +1,54 @@
+package ng.com.uae.kandora.model;
+
+public class Offer {
+    String newPrice,offerExpiry,offerOrNot,specialOffer;
+    SpecialOfferDes specialOfferDes;
+    public Offer(String newPrice,String offerExpiry,String offerOrNot,String specialOffer,SpecialOfferDes specialOfferDes)
+    {
+        this.newPrice = newPrice;
+        this.offerExpiry = offerExpiry;
+        this.offerOrNot = offerOrNot;
+        this.specialOffer = specialOffer;
+        this.specialOfferDes = specialOfferDes;
+    }
+
+    public String getNewPrice() {
+        return newPrice;
+    }
+
+    public void setNewPrice(String newPrice) {
+        this.newPrice = newPrice;
+    }
+
+    public String getOfferExpiry() {
+        return offerExpiry;
+    }
+
+    public void setOfferExpiry(String offerExpiry) {
+        this.offerExpiry = offerExpiry;
+    }
+
+    public String getOfferOrNot() {
+        return offerOrNot;
+    }
+
+    public void setOfferOrNot(String offerOrNot) {
+        this.offerOrNot = offerOrNot;
+    }
+
+    public String getSpecialOffer() {
+        return specialOffer;
+    }
+
+    public void setSpecialOffer(String specialOffer) {
+        this.specialOffer = specialOffer;
+    }
+
+    public SpecialOfferDes getSpecialOfferDes() {
+        return specialOfferDes;
+    }
+
+    public void setSpecialOfferDes(SpecialOfferDes specialOfferDes) {
+        this.specialOfferDes = specialOfferDes;
+    }
+}
